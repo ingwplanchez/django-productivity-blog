cb9d42d44352ff09f044218fe4f9b4dc5d87a24f
Wilmer Planchez <ing.wplanchez@gmail.com>
2025-08-04 18:11:24 -0400

    Modified: Creación de dos modelos principales: Post (para las publicaciones) y Comment (para los comentarios)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+# blog/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post.title}'
