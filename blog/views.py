f9c23aad582a7dc39f876d5ffb4aefce64bffa20
Wilmer Planchez <ing.wplanchez@gmail.com>
2025-08-04 18:35:33 -0400

    Modified: Se agregó la plantilla post_list para mostrar las publicaciones del blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# blog/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-pub_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
