afe6282dd96f64a574f8b034a99e04cad481c6f9
Wilmer Planchez <ing.wplanchez@gmail.com>
2025-08-05 12:13:29 -0400

    Modified: Se agrega un formulario de comentarios para los los artículos publicados

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 # blog/views.py
 
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
+from django.contrib.auth.models import User # Necesitaremos esto para asignar el autor
 
 def post_list(request):
     posts = Post.objects.all().order_by('-pub_date')
@@ -9,5 +11,18 @@ def post_list(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    comments = post.comment_set.all().order_by('-pub_date') # Obtiene los comentarios del post
-    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
+    comments = post.comment_set.all().order_by('-pub_date')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            # Por ahora, asignamos el primer usuario que encontremos, luego lo cambiaremos
+            comment.author = User.objects.first() 
+            comment.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
