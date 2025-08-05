9f78dc162afd0a74fc87e11143bde648bba329f0
Wilmer Planchez <ing.wplanchez@gmail.com>
2025-08-05 12:00:08 -0400

    Modified: Se muestran comentarios en la página de detalles de la publicación

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,5 @@ def post_list(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comment_set.all().order_by('-pub_date') # Obtiene los comentarios del post
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
