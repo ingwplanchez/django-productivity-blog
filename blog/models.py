4ec6c7df13fa22bae05765e1a707337c346e7840
Wilmer Planchez <ing.wplanchez@gmail.com>
2025-08-07 17:54:38 -0400

    Modified: Se agregó un campo de imagen al modelo de publicación y se configuraron archivos multimedia

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
